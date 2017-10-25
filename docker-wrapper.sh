669cba1e191984d584b48a82b86fea70c7361e3d
gitlawr <lawrleegle@gmail.com>
2017-10-25 18:34:32 +0800

    v3 without boot

--- docker-wrapper.sh
+++ docker-wrapper.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+git config --global user.email "ci@rancher.com" \
+&& git config --global user.name "rancher_ci"
+
+# If mount an empty dir, then copy the initial home
+
+DIR="/var/jenkins_home"
+if [ "$(ls -A $DIR)" ];
+then
+     echo "$DIR is not Empty"
+else
+     echo "$DIR is Empty"
+     echo "Start copy jenkins_home by running cp -r /var/rancher/jenkins_home/* $DIR"
+     cp -r /var/rancher/jenkins_home/* $DIR
+     echo "Copy Finishied"
+fi
+
 # If we have docker bind mounted in - no need.
 if (docker version); then
 	echo "Docker is already bind mounted in - we are good to go..."
