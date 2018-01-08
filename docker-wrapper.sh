3a0ae67ecd991201cae14222e493de1a9be0e9e9
Tyson Thomas <tyson.thomas@kadima.consulting>
2018-01-08 19:34:48 +0530

    Updated docker start command

--- docker-wrapper.sh
+++ docker-wrapper.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+docker daemon -H tcp://0.0.0.0:2375 -H unix:///var/run/docker.sock &
 # If we have docker bind mounted in - no need.
 if (docker version); then
 	echo "Docker is already bind mounted in - we are good to go..."
@@ -12,10 +13,10 @@ echo "Launching docker server DND style..."
 # First, make sure that cgroups are mounted correctly.
 CGROUP=/sys/fs/cgroup
 
-[ -d $CGROUP ] || 
+[ -d $CGROUP ] ||
 	mkdir $CGROUP
 
-mountpoint -q $CGROUP || 
+mountpoint -q $CGROUP ||
 	mount -n -t tmpfs -o uid=0,gid=0,mode=0755 cgroup $CGROUP || {
 		echo "Could not make a tmpfs mount. Did you use --privileged?"
 		exit 1
@@ -33,7 +34,7 @@ fi
 for SUBSYS in $(cut -d: -f2 /proc/1/cgroup)
 do
         [ -d $CGROUP/$SUBSYS ] || mkdir $CGROUP/$SUBSYS
-        mountpoint -q $CGROUP/$SUBSYS || 
+        mountpoint -q $CGROUP/$SUBSYS ||
                 mount -n -t cgroup -o $SUBSYS cgroup $CGROUP/$SUBSYS
 
         # The two following sections address a bug which manifests itself
